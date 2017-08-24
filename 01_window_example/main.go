36d23bc8c30865bb5101f7d0e065afcae5968eec
Dave Llewellyn <439837@gmail.com>
2017-08-24 08:58:32 +0100

    Added VSync and window background colour to complete windows example

--- 01_window_example/main.go
+++ 01_window_example/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
-
 
 func main() {
 	pixelgl.Run(run)
@@ -12,14 +12,16 @@ func main() {
 
 func run() {
 	cgf := pixelgl.WindowConfig{
-		Title: "Pixel Rocks!",
+		Title:  "Pixel Rocks!",
 		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true, // VSync aligns the window refresh with your monitor refresh.
 	}
 
 	win, err := pixelgl.NewWindow(cgf)
 	if err != nil {
 		panic(err)
 	}
+	win.Clear(colornames.Blueviolet)
 
 	for !win.Closed() {
 		win.Update()
